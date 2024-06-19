f77147eb6b048d70d538e1ad8ca92bccab7478e7
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2024-06-19 21:26:10 +0200

    19.6.24 working on itemselector

--- Assets/Scripts/InventoryScripts/ScriptableItems/ItemDisplaySelector.cs
+++ Assets/Scripts/InventoryScripts/ScriptableItems/ItemDisplaySelector.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ItemDisplaySelector : MonoBehaviour
 {
     public PowerUp AssignedItem;
+    
+    public Sprite itemImage;
+    public UnityEngine.UI.Image itemDisplay; // Add this line to reference an Image component
 
+    void Update(){
+        itemDisplay.sprite = itemImage;
+    }
     void OnCollisionEnter(Collision other)
     {
         if (!other.gameObject.CompareTag("Player")) return;
